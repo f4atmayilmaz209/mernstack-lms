07210377476b32187a096481aa8a2cb879d6822c
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-01-01 23:33:37 +0300

    fi

--- server/app.ts
+++ server/app.ts
@@ -27,7 +27,9 @@ app.use(cookieParser())
 
 app.use(cors({
     origin:['https://mernstack-lms-soei.vercel.app'],
-    credentials:true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
 }));
 
 //routes
