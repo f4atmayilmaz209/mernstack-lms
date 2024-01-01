a3183db669ab65e0dd24bb5507c45771608897b6
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-01-01 19:37:23 +0300

    vercel

--- server/app.ts
+++ server/app.ts
@@ -23,7 +23,7 @@ app.use(cookieParser())
 //cors=>cross origin resource sharing
 
 app.use(cors({
-    origin:['http://localhost:3000'],
+    origin:['https://mernstack-lms.vercel.app'],
     credentials:true,
 }));
 
