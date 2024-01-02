8420f77c2c32fa2551da9f4c34a25cf84af36408
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-01-03 00:00:12 +0300

    rtt

--- client/next.config.js
+++ client/next.config.js
@@ -1,26 +1,46 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//     images:{
-//         domains:['res.cloudinary.com','randomuser.me'],
-//     },
-//     eslint: { ignoreDuringBuilds: true } 
-// }
-
-// module.exports = nextConfig
-module.exports = {
+const nextConfig = {
+    images:{
+        domains:['res.cloudinary.com','randomuser.me'],
+    },
+    eslint: { ignoreDuringBuilds: true },
     async headers() {
-      return [
-        {
-          // matching all API routes
-          source: "/api/v1/:path*",
-          headers: [
-            { key: "Access-Control-Allow-Credentials", value: "true" },
-            { key: "Access-Control-Allow-Origin", value: "*" },
-            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
-            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
-          ]
-        }
-      ]
-    }
-  };
+    return [
+      {
+
+        source: "/api/v1/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,Origin, X-Requested-With, Content-Type, Accept, Authorization" },
+
+        ]
+      },
+      {
+
+        source: "/api/v1/login/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      },
+      {
+
+        source: "/api/cart/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version,Origin, X-Requested-With, Content-Type, Accept, Authorization" },
+
+        ]
+      },
+    ]
+    },
+}
+
+
   
