6b6c58e9e845d404bcaa31b1a540793f5466ce67
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-01-03 17:24:15 +0300

    w

--- client/next.config.js
+++ client/next.config.js
@@ -42,7 +42,7 @@ const nextConfig = {
         },
         ]
     },
-    pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+    // pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
 }
 
 
