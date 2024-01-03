88e0a790f6e4a758536a7c1642be6cc5d398bfa5
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-01-03 21:36:40 +0300

    qw

--- client/next.config.js
+++ client/next.config.js
@@ -53,6 +53,11 @@ const nextConfig = {
         },
         ]
     },
+    output: 'export',
+    // Optional: Add a trailing slash to all paths `/about` -> `/about/`
+    trailingSlash: true,
+    // Optional: Change the output directory `out` -> `dist`
+    distDir: 'dist',
 
 
 }
