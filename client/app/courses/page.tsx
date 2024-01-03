760b175481f4cb4f37b10c068efc877401d7e418
f4atmayilmaz209 <96868188+f4atmayilmaz209@users.noreply.github.com>
2024-01-03 18:49:05 +0300

    rtt

--- client/app/courses/page.tsx
+++ client/app/courses/page.tsx
@@ -39,68 +39,65 @@ const Page = (props: Props) => {
 
 
   return (
-    // <div>
-    //     {
-    //         isLoading ? (
-    //             <Loader/>
-    //         ):(
-    //             <>
-    //                 <Header
-    //                    route={route}
-    //                    setRoute={setRoute}
-    //                    open={open}
-    //                    setOpen={setOpen}
-    //                    activeItem={1}                    
-    //                 />
-    //                 <div className='w-[95%] 800px:w-[85%] m-auto min-h-[70vh]'>
-    //                     <Heading
-    //                         title={"All courses-ELearning"}
-    //                         description={"Elearning is a programming community."}
-    //                         keywords={"programming community,coding skills,expert insights,collaboration,growth"}
-                        
-    //                     />
-    //                     <br/>
-    //                     <div className='w-full flex items-center flex-wrap'>
-    //                         <div className={`h-[35px] ${category==="All" ? "bg-[crimson]" : "bg-[#5050cb]"} m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`} onClick={()=>setCategory("All")}>
-    //                             All
-    //                         </div>
-    //                         {
-    //                             categories && categories.map((item:any,index:number)=>(
-    //                                 <div key={index}>
-    //                                     <div className={`h-[35px] ${category===item?.title ? "bg-[crimson]" : "bg-[#5050cb]"} m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`} onClick={()=>setCategory(item?.title)}>
-    //                                         {item?.title}
-    //                                     </div>
-    //                                 </div>
-    //                             ))
-    //                         }
-    //                     </div>
-    //                     {
-    //                         courses && courses.length===0 && (
-    //                             <p className={`${styles?.label} justify-center min-h-screen flex items-center`}>
-    //                                 {search ? "No courses found" :"No courses found in this category.Please try another one!"}
-
-    //                             </p>
-    //                         )
-    //                     }
-    //                     <br/>
-    //                     <br/>
-    //                     <div className='grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4'>
-    //                         {
-    //                             courses && courses.map((item:any,index:number)=>(
-    //                                 <CourseCard item={item} key={index}/>
-    //                             ))
-    //                         }
-    //                     </div>
-
-
-    //                 </div>
-    //                 <Footer/> 
-    //             </>
-    //         )
-    //     }
-    // </div>
     <div>
-        çello
+        {
+            isLoading ? (
+                <Loader/>
+            ):(
+                <>
+                    <Header
+                       route={route}
+                       setRoute={setRoute}
+                       open={open}
+                       setOpen={setOpen}
+                       activeItem={1}                    
+                    />
+                    <div className='w-[95%] 800px:w-[85%] m-auto min-h-[70vh]'>
+                        <Heading
+                            title={"All courses-ELearning"}
+                            description={"Elearning is a programming community."}
+                            keywords={"programming community,coding skills,expert insights,collaboration,growth"}
+                        
+                        />
+                        <br/>
+                        <div className='w-full flex items-center flex-wrap'>
+                            <div className={`h-[35px] ${category==="All" ? "bg-[crimson]" : "bg-[#5050cb]"} m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`} onClick={()=>setCategory("All")}>
+                                All
+                            </div>
+                            {
+                                categories && categories.map((item:any,index:number)=>(
+                                    <div key={index}>
+                                        <div className={`h-[35px] ${category===item?.title ? "bg-[crimson]" : "bg-[#5050cb]"} m-3 px-3 rounded-[30px] flex items-center justify-center font-Poppins cursor-pointer`} onClick={()=>setCategory(item?.title)}>
+                                            {item?.title}
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                        {
+                            courses && courses.length===0 && (
+                                <p className={`${styles.label} justify-center min-h-screen flex items-center`}>
+                                    {search ? "No courses found" :"No courses found in this category.Please try another one!"}
+
+                                </p>
+                            )
+                        }
+                        <br/>
+                        <br/>
+                        <div className='grid grid-cols-1 gap-[20px] md:grid-cols-2 md:gap-[25px] lg:grid-cols-3 lg:gap-[25px] 1500px:grid-cols-4'>
+                            {
+                                courses && courses.map((item:any,index:number)=>(
+                                    <CourseCard item={item} key={index}/>
+                                ))
+                            }
+                        </div>
+
+
+                    </div>
+                    <Footer/> 
+                </>
+            )
+        }
     </div>
   )
 }
